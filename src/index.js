9ec18a0ed4329f7b807a9b6a02f57fad0e3cd101
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2025-06-23 23:37:37 +0800

    Import index.css

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { BrowserRouter } from 'react-router'
 
 import App from './App'
 
+import './index.css'
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
     <React.StrictMode>
