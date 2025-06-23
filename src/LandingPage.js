fb648bd09128b67a11f4eb4fdf8f9c3f4ed09553
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2025-06-23 22:56:58 +0800

    Create HTML of LandingPage()

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,5 +1,22 @@
+import { ReactComponent as Logo } from './logo.svg'
+
 function LandingPage() {
-    return <></>
+    return (
+        <div>
+            <div>
+                <Logo />
+                <span>ThinkTalk</span>
+                <button type="button">
+                    <span>Log Out</span>
+                </button>
+            </div>
+            <div>
+                <span>
+                    A social media platform where you think before you talk.
+                </span>
+            </div>
+        </div>
+    )
 }
 
 export default LandingPage
