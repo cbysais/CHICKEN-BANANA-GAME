03a3cd37d77386c3d61e13f89af5dbb43a4032db
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2025-06-23 22:50:42 +0800

    Route LandingPage() to /landing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router'
 
 import LoginPage from './LoginPage'
+import LandingPage from './LandingPage'
 
 function App() {
     return (
         <Routes>
             <Route path="/" element={<LoginPage />} />
+            <Route path="/landing" element={<LandingPage />} />
         </Routes>
     )
 }
