85370f3b3c867211be63147668180574bb06d19f
cbysais <217733506+cbysais@users.noreply.github.com>
2025-06-24 23:26:10 +0800

    Add woman.jpg

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,6 +1,7 @@
 import { useNavigate } from 'react-router'
 
 import { ReactComponent as Logo } from './logo.svg'
+import woman from './woman.jpg'
 
 function LandingPage() {
     const navigate = useNavigate()
@@ -10,7 +11,7 @@ function LandingPage() {
     }
 
     return (
-        <div className="flex h-screen w-screen flex-col">
+        <div className="flex h-screen min-h-screen w-screen flex-col">
             <div className="flex h-fit w-full items-center justify-between bg-gradient-to-tr from-blue-400 to-blue-500 px-4 py-4">
                 <Logo className="h-12 w-12 fill-blue-50" />
                 <span className="select-none text-5xl font-black text-blue-50">
@@ -26,12 +27,13 @@ function LandingPage() {
                     </span>
                 </button>
             </div>
-            <div className="flex h-full w-full bg-gradient-to-tr from-white to-blue-50 px-8 py-8">
-                <span className="bg-gradient-to-tr from-blue-300 to-blue-500 bg-clip-text text-9xl font-bold text-transparent">
+            <div className="flex w-full flex-grow items-center bg-gradient-to-tr from-white to-blue-50 pl-8 pt-8">
+                <span className="bg-gradient-to-tr from-blue-300 to-blue-500 bg-clip-text text-7xl font-bold text-transparent sm:text-8xl lg:text-9xl">
                     A social media platform where you{' '}
                     <span className="text-blue-500">THINK</span> before you{' '}
                     <span className="text-blue-500">TALK</span>.
                 </span>
+                <div className="hidden h-full min-w-[calc((1/2)*100%)] rounded-tl-2xl bg-[url(woman.jpg)] lg:flex"></div>
             </div>
         </div>
     )
