5c3e6b48a7620e5e5f030f831e79949880086de7
cbysais <217733506+cbysais@users.noreply.github.com>
2025-06-24 21:08:28 +0800

    Create CSS of LandingPage()

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -10,16 +10,24 @@ function LandingPage() {
     }
 
     return (
-        <div>
-            <div>
-                <Logo />
-                <span>ThinkTalk</span>
-                <button type="button" onClick={logIn}>
-                    <span>Log In</span>
+        <div className="flex h-screen w-screen flex-col">
+            <div className="flex h-fit w-full items-center justify-between bg-gradient-to-tr from-blue-400 to-blue-500 px-4 py-4">
+                <Logo className="h-12 w-12 fill-blue-50" />
+                <span className="cursor-default text-5xl font-black text-blue-50">
+                    ThinkTalk
+                </span>
+                <button
+                    className="group rounded-full bg-blue-50 px-6 py-2"
+                    type="button"
+                    onClick={logIn}
+                >
+                    <span className="text-2xl font-bold text-blue-500 group-hover:underline">
+                        Log In
+                    </span>
                 </button>
             </div>
-            <div>
-                <span>
+            <div className="flex h-full w-full bg-gradient-to-tr from-white to-blue-50 px-8 py-8">
+                <span className="bg-gradient-to-tr from-blue-300 to-blue-500 bg-clip-text text-9xl font-bold text-transparent">
                     A social media platform where you think before you talk.
                 </span>
             </div>
