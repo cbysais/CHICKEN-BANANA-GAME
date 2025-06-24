4d49ccc78982322705ee2bdb4c3743d33317835f
cbysais <217733506+cbysais@users.noreply.github.com>
2025-06-24 22:21:17 +0800

    Use select-none instead of cursor-default

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -13,7 +13,7 @@ function LandingPage() {
         <div className="flex h-screen w-screen flex-col">
             <div className="flex h-fit w-full items-center justify-between bg-gradient-to-tr from-blue-400 to-blue-500 px-4 py-4">
                 <Logo className="h-12 w-12 fill-blue-50" />
-                <span className="cursor-default text-5xl font-black text-blue-50">
+                <span className="select-none text-5xl font-black text-blue-50">
                     ThinkTalk
                 </span>
                 <button
