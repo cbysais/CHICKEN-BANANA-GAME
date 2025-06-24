599b2763ccc0c69fb095af02b7a45416818809de
cbysais <217733506+cbysais@users.noreply.github.com>
2025-06-24 22:17:39 +0800

    Create CSS of LoginPage()

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -7,9 +7,10 @@ function LoginPage() {
     const [password, setPassword] = useState('')
 
     return (
-        <div>
-            <form>
+        <div className="flex h-screen w-screen flex-col items-center justify-center gap-y-8 bg-gradient-to-bl from-white to-blue-50 px-16 md:flex-row-reverse md:gap-x-16">
+            <form className="flex h-fit w-96 flex-col gap-y-4 rounded-2xl bg-white px-4 py-4 shadow-md">
                 <input
+                    className="w-full rounded-2xl border border-blue-300 bg-white px-2 py-2"
                     type="text"
                     autoComplete="username"
                     placeholder="Username"
@@ -19,6 +20,7 @@ function LoginPage() {
                     }}
                 />
                 <input
+                    className="w-full rounded-2xl border border-blue-300 bg-white px-2 py-2"
                     type="password"
                     autoComplete="current-password"
                     placeholder="Password"
@@ -27,13 +29,20 @@ function LoginPage() {
                         setPassword(e.target.value)
                     }}
                 />
-                <button type="button">
-                    <span> Log In</span>
+                <button
+                    className="w-full rounded-2xl bg-blue-500 px-2 py-2 hover:bg-blue-600"
+                    type="button"
+                >
+                    <span className="text-2xl font-bold text-blue-50">
+                        Log In
+                    </span>
                 </button>
             </form>
-            <div>
-                <Logo />
-                <span>ThinkTalk</span>
+            <div className="flex flex-col items-center">
+                <Logo className="h-32 w-32 fill-blue-500" />
+                <span className="select-none text-2xl font-black text-blue-500">
+                    ThinkTalk
+                </span>
             </div>
         </div>
     )
