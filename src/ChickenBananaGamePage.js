ba8c9e64c2367340eff6db26c3d44a8e0305e838
cbysais <217733506+cbysais@users.noreply.github.com>
2025-06-28 16:25:51 +0800

    Create createBalancedRandomIntegers()

--- src/ChickenBananaGamePage.js
+++ src/ChickenBananaGamePage.js
@@ -1,4 +1,32 @@
+function createBalancedRandomIntegers(numberOfSquares) {
+    // numberOfSquares must be even.
+    const zeroes = Array(numberOfSquares / 2).fill(0)
+    const ones = Array(numberOfSquares / 2).fill(1)
+
+    const balancedIntegers = zeroes.concat(ones)
+
+    // Schwartzian transform JavaScript implementation from https://stackoverflow.com/a/46545530
+    const balancedRandomIntegers = balancedIntegers
+        .map((integer) => {
+            return { integer, randomValue: Math.random() }
+        })
+        .sort((a, b) => {
+            return a.randomValue - b.randomValue
+        })
+        .map(({ integer }) => {
+            return integer
+        })
+
+    return balancedRandomIntegers
+}
+
 function ChickenBananaGamePage() {
+    // inputtedNumberOfSquares must be even.
+    const inputtedNumberOfSquares = 6 * 6
+    // numberOfSquares is created as a fail-safe for odd inputtedNumberOfSquares.
+    const numberOfSquares = Math.floor(inputtedNumberOfSquares / 2) * 2
+    const balancedRandomIntegers = createBalancedRandomIntegers(numberOfSquares)
+
     return <></>
 }
 
